766a12927daed7ebfd99bf7b2a1ebd703b116dd2
rryabinin <ninibayrr@mail.ru>
2025-02-26 16:26:07 +0300

    folders

--- src/main/java/ru/ispo/music_sevice/MusicServiceApplication.java
+++ src/main/java/ru/ispo/music_sevice/MusicServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusicSeviceApplication {
+public class MusicServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MusicSeviceApplication.class, args);
+		SpringApplication.run(MusicServiceApplication.class, args);
 	}
 
 }
